9c9769d6a7b91b7a18acc041638ef3075972863f
nzhao <nzhao@csrc.ac.cn>
2016-01-04 20:44:35 +0800

    SpinSystem.

--- src/include/spin/SpinSystem.h
+++ src/include/spin/SpinSystem.h
@@ -1,11 +1,28 @@
 #ifndef SPINSYSTEM_H
 #define SPINSYSTEM_H
+#include "include/spin/spin.h"
+#include "include/spin/SpinInteraction.h"
+#include "include/spin/SpinInteractionDefine.h"
+#include "include/kron/KronProd.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 //{{{ cSpinSystem
 class cSpinSystem
 {
-}
+public:
+    cSpinSystem();
+    cSpinSystem(const vector<cSPIN>& spin_list);
+    ~cSpinSystem();
+
+    void addSpinInteraction(cSpinInteraction& spin_interaction);
+    SumKronProd getSumKronOperator();
+
+protected:
+private:
+    DIM_LIST _dim_list;
+    vector<cSPIN> _spin_list;
+    vector<cSpinInteraction> _spin_interaction_list;
+};
 //}}}
 ////////////////////////////////////////////////////////////////////////////////
 #endif
--- src/source/spin/SpinSystem/SpinSystem.cpp
+++ src/source/spin/SpinSystem/SpinSystem.cpp
@@ -1,2 +1,40 @@
 #include <vector>
+#include <assert.h>
 #include "include/spin/SpinSystem.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+//{{{ cSpinSystem
+cSpinSystem::cSpinSystem()
+{
+    cout << "default constructor of cSpinSystem." << endl;
+}
+cSpinSystem::cSpinSystem(const vector<cSPIN>& spin_list)
+{
+    cout << "constructor of cSpinSystem with spin_list." << endl;
+    _spin_list = spin_list;
+
+    for(auto spin: _spin_list)
+        _dim_list.push_back( spin.get_dimension() );
+}
+cSpinSystem::~cSpinSystem()
+{
+    cout << "destructor of cSpinSystem." << endl;
+}
+
+void cSpinSystem::addSpinInteraction(cSpinInteraction& spin_interaction)
+{
+    assert( this->_dim_list == spin_interaction.getDimList() );
+    _spin_interaction_list.push_back(spin_interaction);
+}
+
+SumKronProd cSpinSystem::getSumKronOperator()
+{
+    SumKronProd res=_spin_interaction_list[0].getSumKronProd();
+    for(int i=1; i<_spin_interaction_list.size(); ++i)
+        res=res+_spin_interaction_list[i].getSumKronProd();
+    return res;
+}
+
+//}}}
+////////////////////////////////////////////////////////////////////////////////
