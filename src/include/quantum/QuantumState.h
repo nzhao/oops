29f4448234957914c763ac6a73ac67c4bc7610a4
nzhao <nzhao@csrc.ac.cn>
2016-01-12 09:09:38 +0800

    QuantumState Class def.

--- src/include/quantum/QuantumState.h
+++ src/include/quantum/QuantumState.h
@@ -1,20 +1,26 @@
 #ifndef QUANTUMSTATE_H
 #define QUANTUMSTATE_H
 
+#include "include/easylogging++.h"
 /// \addtogroup Quantum 
 /// @{
 
 /// \defgroup QuantumState QuantumState
 /// @{
 
+
 ////////////////////////////////////////////////////////////////////////////////
 //{{{ QuantumState
 class QuantumState
 {
 public:
+    QuantumState();
+    ~QuantumState();
 protected:
+    bool _is_pure;
+    int _dimension;
 private:
-}
+};
 //}}}
 ////////////////////////////////////////////////////////////////////////////////
 
