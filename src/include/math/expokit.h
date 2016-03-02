76913d86e5ed955c0d9b1759eba54cc9847f0abb
whhu <whhu@csrc.ac.cn>
2016-03-02 19:20:42 +0800

    add Pade approximation to MatExp

---
+++ src/include/math/expokit.h
@@ -0,0 +1,11 @@
+#ifdef __cplusplus
+extern"C" {
+#endif
+
+// irreducible rational Pade approximation in Expokit;
+void zgpadm_(int *ideg, int *m, double *t, std::complex<double> *H, int *ldh, std::complex<double> *wsp, int *lwsp, int *ipiv, int *iexph, int *ns, int *iflag);
+
+#ifdef __cplusplus
+}
+#endif
+
