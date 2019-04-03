b6215544bc32866837c5c3fdc33517398e548e03
Dawu Xiao <xiaodw@csrc.ac.cn>
2019-04-03 09:05:06 +0800

    YSODefectCenter
    Add DefectCenter and SpinData

--- src/source/spin/Spin/SpinData.cpp
+++ src/source/spin/Spin/SpinData.cpp
@@ -6,6 +6,8 @@ SpinProperty N14      = {3,  1.9337792e7,    2.96,    0.0};
 SpinProperty N15      = {2, -2.71261804e7,   0.0,      0.0};
 SpinProperty ELECTRON = {2, -1.760859708e11, 0.0,      0.0};
 SpinProperty NVE      = {3, -1.760859708e11, 2.87e3,   0.0};
+SpinProperty Y        = {2, -1.3155e7,       0.0,      0.0};
+SpinProperty Si       = {2, -5.3188e7,       0.0,      0.0};
 ////////////////////////////////////////////////////////////////////////////////
 //{{{ SPINDATA
 cSPINDATA::cSPINDATA()
@@ -15,7 +17,8 @@ cSPINDATA::cSPINDATA()
     data["15C"]=N15;
     data["E"]=ELECTRON;
     data["NVe"]=NVE;
+    data["Y"]=Y;
+    data["Si"]=Si;
 }
 //}}}
 ////////////////////////////////////////////////////////////////////////////////
-
